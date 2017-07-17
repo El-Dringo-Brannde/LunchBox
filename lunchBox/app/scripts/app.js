a260b8bfed07ac56076119bfe1906ad568f4a2cc
Brandon Lee Dring <Brandon.d.junk@gmail.com>
2017-07-17 14:27:57 -0700

    Integrated login page port

--- lunchBox/app/scripts/app.js
+++ lunchBox/app/scripts/app.js
@@ -9,28 +9,33 @@
  * Main module of the application.
  */
 angular
-  .module('lunchBoxApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+   .module('lunchBoxApp', [
+      'ngAnimate',
+      'ngCookies',
+      'ngResource',
+      'ngRoute',
+      'ngSanitize',
+      'ngTouch'
+   ])
+   .config(function($routeProvider, $locationProvider) {
+      $routeProvider
+         .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl',
+            controllerAs: 'main'
+         })
+         .when('/about', {
+            templateUrl: 'views/about.html',
+            controller: 'AboutCtrl',
+            controllerAs: 'about'
+         })
+         .when("/login", {
+            templateUrl: 'views/login.html',
+            controller: 'loginController',
+            controllerAs: 'login'
+         })
+         .otherwise({
+            redirectTo: '/'
+         });
       $locationProvider.hashPrefix('');
-  });
+   });
