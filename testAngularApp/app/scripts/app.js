159514d66c2e8ca17ec1af9fb43289955825b857
Roche, Devin <Devin.Roche@cdk.com>
2017-07-17 13:26:53 -0700

    nuke poop
    yeoman angular project

---
+++ testAngularApp/app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name lunchBoxApp
+ * @description
+ * # lunchBoxApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('lunchBoxApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+      $locationProvider.hashPrefix('');
+  });
