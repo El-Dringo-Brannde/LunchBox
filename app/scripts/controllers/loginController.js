861af9e66ea9f197869b8f84af76b23fe2b6440d
Roche, Devin <Devin.Roche@cdk.com>
2017-07-17 16:05:09 -0700

    nav show/hide

--- app/scripts/controllers/loginController.js
+++ app/scripts/controllers/loginController.js
@@ -11,6 +11,7 @@ angular.module('lunchBoxApp')
    .controller('loginController', function($scope, $http, $cookies, $window, commService) {
       $scope.loginName = "";
       $scope.currentUser = "";
+      $scope.userLogin = false;
       // create a message to display in our view 
       $scope.message = 'Everyone come and see how good I look!';
       $scope.signIn = function() {
@@ -23,11 +24,16 @@ angular.module('lunchBoxApp')
                url: 'http://ffg.cdk.com:4000/find/user/' + $scope.loginName
             }).then(function success(response) {
                if ($scope.currentUser = response.data.user.length == 0) {
+                  alert('invalid username');
                } else if(response.data.user.length > 1){
                   for(var i=0; i<response.data.user.length; i++){
                      if(response.data.user[i].sAMAccountName == $scope.loginName){
                         $scope.currentUser = response.data.user[i].cn
                         $cookies.put("user", $scope.currentUser)
+                        commService.set({
+                           name: $scope.currentUser,
+                           showNav: true
+                        })
                         $window.location.href = '/#/';
                      }
                   }
@@ -35,7 +41,8 @@ angular.module('lunchBoxApp')
                   $scope.currentUser = response.data.user[0].cn
                   $cookies.put("user", $scope.currentUser)
                   commService.set({
-                     name: $scope.currentUser
+                     name: $scope.currentUser,
+                     showNav: true
                   });
 
                   $window.location.href = '/#/';
@@ -43,4 +50,5 @@ angular.module('lunchBoxApp')
             })
          }
       }
+      
    });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,6 +14,8 @@ angular.module('lunchBoxApp')
          alert("You have been logged out!")
          $window.location.href = '/#/login';
       }
+      $scope.userLogin = commService.get().showNav
+      console.log($scope.userLogin)
       $scope.user = commService.get().name.split(",").pop()
-
+      console.log($scope.user)
    });
