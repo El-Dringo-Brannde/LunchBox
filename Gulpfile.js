11b1b7e9943f3005550d0f4bb0ec150ffa8df5cd
Christensen, Connor <Connor.Christensen@cdk.com>
2017-07-17 11:42:28 -0700

    updated a gulp build sequence

--- Gulpfile.js
+++ Gulpfile.js
@@ -12,9 +12,12 @@ var dest = require('gulp-dest');
 //include the autoprefixer (that auto creates support for old browsers)
 var autoprefixer = require('gulp-autoprefixer');
 
-browserSync = require('browser-sync').create(), //browser sync and runs a static server
+var runSequence = require('run-sequence'); // runs tasks in a sequnce
 
-reload = browserSync.reload;
+//browser sync and runs a static server
+var browserSync = require('browser-sync').create();
+
+var reload = browserSync.reload;
 
 
 //set the autoprefixer to work on
@@ -22,75 +25,83 @@ reload = browserSync.reload;
 //  - all browsers that have more than 5% market share
 //  - firefox ESR
 var autoprefixerOptions = {
-    browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+   browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
 };
 
 
 //handle an error instead of puking and dying
 function handleError(error) {
-    console.log(error.toString());
-    this.emit('end');
+   console.log(error.toString());
+   this.emit('end');
 }
 
 var paths = {
-    jsWatch:   '**/*.js',
-    scssWatch: '**/*.scss',
-    htmlWatch: '**/*.html',
+   jsWatch: '**/*.js',
+   scssWatch: '**/*.scss',
+   htmlWatch: '**/*.html',
 
-    scssComplile: 'ClientSide/scss/*.scss',
-    htmlCompile:  'ClientSide/html/*.html'
+   scssComplile: 'ClientSide/scss/*.scss',
+   htmlCompile: 'ClientSide/html/*.html'
 }
 
 
 //this will compile all our scss files
-gulp.task('scss', function() {
-    return gulp
-        .src(paths.scssComplile)
-        .pipe(scss())
-        .pipe(autoprefixer())
-        .pipe(dest('product', {
-            ext: '.css'
-        }))
-        .pipe(gulp.dest('./'));
+gulp.task('scss', function () {
+   return gulp
+      .src(paths.scssComplile)
+      .pipe(scss())
+      .pipe(autoprefixer())
+      .pipe(dest('product', {
+         ext: '.css'
+      }))
+      .pipe(gulp.dest('./'));
 });
 
-gulp.task('html', function() {
-    return gulp
-        .src(paths.htmlCompile)
-        .pipe(dest('product'))
-        .pipe(gulp.dest('./'));
+gulp.task('html', function () {
+   return gulp
+      .src(paths.htmlCompile)
+      .pipe(dest('product'))
+      .pipe(gulp.dest('./'));
 });
 
 
-gulp.task('js', function() {
-    return;
+gulp.task('js', function () {
+   return;
 })
 
 var config = {
-    port: 8080,
-    browser: "google chrome",
-    devBaseUrl: 'http://localhost'
+   port: 8080,
+   browser: "google chrome",
+   devBaseUrl: 'http://localhost'
 }
 
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "dist",
-            index: "index.html",
-            directory: false
-        },
-        port: config.port,
-        open: "local",
-        browser: config.browser,
-        reloadOnRestart: true,
-        notify: false,
-        injectChanges: true
-    });
+gulp.task('browser-sync', function () {
+   browserSync.init({
+      server: {
+         baseDir: "dist",
+         index: "index.html",
+         directory: false
+      },
+      port: config.port,
+      open: "local",
+      browser: config.browser,
+      reloadOnRestart: true,
+      notify: false,
+      injectChanges: true
+   });
+});
+
+
+gulp.task('build', function () {
+   runSequence(
+      'scss',
+      'html'
+   );
 });
 
 
 //gulp watch will do all the checking and work for us
-gulp.task('default', function() {
-    gulp.watch(paths.scssWatch, ['scss']);
-    gulp.watch(paths.htmlWatch, ['html']);
+gulp.task('default', function () {
+   gulp.watch(paths.scssWatch, ['scss']);
+   gulp.watch(paths.htmlWatch, ['html']);
 });
