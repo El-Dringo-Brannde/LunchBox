8eb956b597c8cd5c3992607a7690976d943d9974
Brandon Lee Dring <Brandon.d.junk@gmail.com>
2017-07-17 09:32:07 -0700

    More test files

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express")
 const app = express()
+const path = require('path');
+app.use(express.static(__dirname))
+
+app.get("/:route/:name", function(req, res) {
+  console.log(req.params.name, req.params.route)
+  res.sendFile(path.join(__dirname + '/ClientSide/' + req.params.route + '/' + req.params.name))
+})
 
 app.get("/", function(req, res) {
-  res.send("Hello World!")
-})
+  res.sendFile(path.join(__dirname + '/ClientSide/html/home.html'))
+});
 
 app.listen(3000, function() {
   console.log("Server running at port 3000...")
-})
+});
