9c1761b7503b2ab175b67b3f71358bd5c17a9fdd
Kisumi <elaineyangx@gmail.com>
2017-07-18 16:11:55 -0700

    added map that finds restaurants

--- app/scripts/map.js
+++ app/scripts/map.js
@@ -1,27 +1,46 @@
-/* global google, center, getMap */
-
-'use strict';
-
-var map, center;
-
-function createMap(position, zoomLevel) {
-   // Create a map object and specify the DOM element for display.
-   map = new google.maps.Map(document.getElementById('map'), {
-      center: position,
-      scrollwheel: true,
-      draggable: true,
-      zoom: zoomLevel
-   });
-   var marker = new google.maps.Marker({
-      position: position,
-      map: map
-   });
-}
+var map;
+var infowindow;
 
 function initMap() {
-   var CDK_Portland = {
+   'use strict'
+   var portland = {
       lat: 45.504023,
       lng: -122.679433
    };
-   createMap(CDK_Portland, 15);
+
+   map = new google.maps.Map(document.getElementById('map'), {
+      center: portland,
+      zoom: 17
+   });
+
+   infowindow = new google.maps.InfoWindow();
+   var service = new google.maps.places.PlacesService(map);
+   service.nearbySearch({
+      location: portland,
+      radius: 5000,
+      type: ['restaurant']
+   }, callback);
+}
+
+function callback(results, status) {
+   'use strict'
+   if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+         createMarker(results[i]);
+      }
+   }
+}
+
+function createMarker(place) {
+   'use strict'
+   var placeLoc = place.geometry.location;
+   var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location
+   });
+
+   google.maps.event.addListener(marker, 'click', function () {
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
+   });
 }
