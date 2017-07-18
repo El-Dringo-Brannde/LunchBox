24863dd6a327a5699a3fbdd0252abd89ef49722c
Christensen, Connor <Connor.Christensen@cdk.com>
2017-07-18 10:22:23 -0700

    fixed all warnings in loginController

--- app/scripts/controllers/loginController.js
+++ app/scripts/controllers/loginController.js
@@ -1,3 +1,5 @@
+/* global alert */
+
 'use strict';
 
 /**
@@ -12,44 +14,42 @@ angular.module('lunchBoxApp')
       $scope.loginName = "";
       $scope.currentUser = "";
       $scope.userLogin = false;
-      $rootScope.$broadcast("hideNav")
+      $rootScope.$broadcast("hideNav");
       // create a message to display in our view 
       $scope.message = 'Everyone come and see how good I look!';
       $scope.signIn = function() {
-         if ($scope.loginName.length == 0) {
-            console.log("Invalid Username")
+         if ($scope.loginName.length === 0) {
+            console.log("Invalid Username");
          } else {
-            console.log("success, submitting request")
+            console.log("success, submitting request");
             $scope.request = $http({
                method: 'GET',
                url: 'http://ffg.cdk.com:4000/find/user/' + $scope.loginName
             }).then(function success(response) {
-               if ($scope.currentUser = response.data.user.length == 0) {
+               if (response.data.user.length === 0) {
                   alert('invalid username');
                } else if (response.data.user.length > 1) {
                   for (var i = 0; i < response.data.user.length; i++) {
-                     if (response.data.user[i].sAMAccountName == $scope.loginName) {
-                        $scope.currentUser = response.data.user[i].cn
-                        $cookies.put("user", $scope.currentUser)
+                     if (response.data.user[i].sAMAccountName === $scope.loginName) {
+                        $scope.currentUser = response.data.user[i].cn;
+                        $cookies.put("user", $scope.currentUser);
                         commService.set({
                            name: $scope.currentUser,
                            showNav: true
-                        })
+                        });
                         $window.location.href = '/#/';
                      }
                   }
                } else {
-                  $scope.currentUser = response.data.user[0].cn
-                  $cookies.put("user", $scope.currentUser)
+                  $scope.currentUser = response.data.user[0].cn;
+                  $cookies.put("user", $scope.currentUser);
                   commService.set({
                      name: $scope.currentUser,
                      showNav: true
                   });
-
                   $window.location.href = '/#/';
                }
-            })
+            });
          }
-      }
-
+      };
    });
