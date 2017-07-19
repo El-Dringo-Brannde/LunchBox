785b893331f531ea15b3f3a67f90370f38da0c4b
Christensen, Connor <Connor.Christensen@cdk.com>
2017-07-19 10:36:38 -0700

    added regex matching and style changes to posting event form

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,5 @@
 //global variables go here to suppress warnings
-/*global alert*/
+/*global alert, $*/
 
 'use strict';
 
@@ -12,8 +12,8 @@
  */
 const zomatoKey = "e52fff3091a307dca21f7c48b4796345";
 angular.module('lunchBoxApp')
-   .controller('MainCtrl', function ($scope, $cookies, $window, commService, $rootScope, $http) {
-
+   .controller('MainCtrl', function ($scope, $cookies, $window, commService, $rootScope, $http, navbar) {
+      navbar()
       var baseUrl = "http://localhost:3005/";
 
       if ($cookies.get("user") == undefined) {
@@ -26,8 +26,8 @@ angular.module('lunchBoxApp')
       } else {
          userName = commService.get().name;
       }
-      $scope.user = userName.split(",").pop();
-      $scope.user != null ? $scope.userLogin : false;
+      $scope.user = userName.split(",").pop()
+      $scope.user != null ? $scope.userLogin : false
       $rootScope.$broadcast("showNav");
 
 
@@ -44,9 +44,9 @@ angular.module('lunchBoxApp')
       //the createEvent function is called on form submission
       $scope.createEvent = function () {
          //assign to temp variables for easy readibility
-         var name     = $scope.restaurant.name,
-            address   = $scope.restaurant.address,
-            time      = $scope.time,
+         var name = $scope.restaurant.name,
+            address = $scope.restaurant.address,
+            time = $scope.time,
             transport = $scope.transport;
 
          if (name === "" || address === "" || time === "" || transport === "") {
@@ -57,31 +57,9 @@ angular.module('lunchBoxApp')
 
             //TODO: some validation before the object is created
 
-
-            /*
-            username: obj.username,
-            fullName: obj.full,
-            email: obj.mail,
-            favorites: [],
-            friends: [],
-            peopleGoing: [],
-            looking: "false",
-            restaurant: {
-               name: "",
-               address: ""
-            },
-            time: "",
-            travelMethod: ""
-            */
-
             var submissionObject = {
                //get the username from whatever thing is keeping the user logged in
-               
-               //uncomment this when the cookie is properly set
-               //username: $cookies.get("user").userName,
-               
-               //hardcoded value
-               username: "dringb",
+               username: $cookies.getObject("user").userName,
                restaurant: {
                   name: name,
                   address: address
@@ -89,23 +67,53 @@ angular.module('lunchBoxApp')
                time: time,
                travelMethod: transport
             };
-         
-            console.log(submissionObject);
-         
-            $scope.request = $http.put(baseUrl + "goingSomewhere", submissionObject).then(function success(response) {
-               
-               console.log(response);
-               
-               //clear all the input fields after the data has been put in the database
-               $scope.restaurant.name = "";
-               $scope.restaurant.address = "";
-               $scope.time = "";
-               $scope.tranport = "";
-            },
-            function failiure(response) {
-               console.log("there was an error posting the data");
-               $scope.submissionError = "there was an error posting the data";
-            });
+            $scope.request = $http.put(baseUrl + "goingSomewhere", submissionObject)
+               .then(function success(response) {
+                     //clear all the input fields after the data has been put in the database
+                     $scope.restaurant.name = "";
+                     $scope.restaurant.address = "";
+                     $scope.time = "";
+                     $scope.tranport = "";
+                  },
+                  function failiure(response) {
+                     console.log("there was an error posting the data");
+                     $scope.submissionError = "there was an error posting the data";
+                  });
          }
       };
+   
+      function testRegex(regex, value, tag) {
+         
+         var errorColor = "red";
+         
+         //if the time matches the regular expression
+         if (regex.test(value)) {
+            //leave the text be whatever color it is normally
+            $(tag).css("color", "");
+            return true;
+         }
+         else {
+            $(tag).css("color", errorColor);
+            return false;
+         }
+      }
+   
+      $("#timeInput").on("focusout", function () {
+         var userValue = $("#timeInput").val();
+         
+         //match either one or two numbers, then a :, then two numbers
+         var timeRule = new RegExp("^[0-9]{1,2}:[0-9]{2}$");
+         
+         if (testRegex(timeRule, userValue, "#timeInput") || userValue === "") {
+            $("#error").html("");
+         }
+         else {
+            $("#error").html("the departure time does not match the required format");
+         }
+         
+      });
+   
+      
+      
+      
    });
