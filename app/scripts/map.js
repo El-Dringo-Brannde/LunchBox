5af187ac4dd6f13cf773d1043aab6ab7eea74649
Kisumi <elaineyangx@gmail.com>
2017-07-18 17:00:27 -0700

    removed unnecessary points of interest from map

--- app/scripts/map.js
+++ app/scripts/map.js
@@ -1,20 +1,36 @@
 var map;
 var infowindow;
+var portland;
+var service;
+
+var theStyle = [
+   {
+      featureType: "poi",
+      elementType: "labels",
+      stylers: [
+         {
+            visibility: "off"
+         }
+      ]
+   }
+];
 
 function initMap() {
-   'use strict'
-   var portland = {
+   'use strict';
+   portland = {
       lat: 45.504023,
       lng: -122.679433
    };
 
    map = new google.maps.Map(document.getElementById('map'), {
       center: portland,
-      zoom: 14
+      zoom: 14,
    });
 
+   map.set('styles', theStyle)
+   
    infowindow = new google.maps.InfoWindow();
-   var service = new google.maps.places.PlacesService(map);
+   service = new google.maps.places.PlacesService(map);
    service.nearbySearch({
       location: portland,
       radius: 5000,
@@ -23,7 +39,7 @@ function initMap() {
 }
 
 function callback(results, status) {
-   'use strict'
+   'use strict';
    if (status === google.maps.places.PlacesServiceStatus.OK) {
       for (var i = 0; i < results.length; i++) {
          createMarker(results[i]);
@@ -32,7 +48,7 @@ function callback(results, status) {
 }
 
 function createMarker(place) {
-   'use strict'
+   'use strict';
    var placeLoc = place.geometry.location;
    var marker = new google.maps.Marker({
       map: map,
