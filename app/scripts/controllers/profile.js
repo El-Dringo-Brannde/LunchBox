068caa917389c2d23cfa6c7388e62bd1a33ebfd2
Roche, Devin <Devin.Roche@cdk.com>
2017-07-24 10:34:52 -0700

    console.log test
    testesteestsetestestetstetestetst

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -10,129 +10,129 @@
  * Controller of the lunchBoxApp
  */
 angular.module('lunchBoxApp')
-   .controller('ProfileCtrl', function ($scope, $cookies, $http, commService, $rootScope, navbar) {
-      $(".navBtn").each((i, ele) => $(ele).css("color", "#578bff"));
-      $("#profileLink").css("color", "#82c600");
-      var baseUrl = "http://localhost:3005/";
-      $scope.foundPhoto = false;
+  .controller('ProfileCtrl', function ($scope, $cookies, $http, commService, $rootScope, navbar) {
+    $(".navBtn").each((i, ele) => $(ele).css("color", "#578bff"));
+    $("#profileLink").css("color", "#82c600");
+    var baseUrl = "http://localhost:3005/";
+    $scope.foundPhoto = false;
 
-      navbar();
-      var curUser = $cookies.getObject("user").full.split(",");
-      var curUserName = $cookies.getObject("user").userName.split(",");
-      $scope.user = curUser.pop();
+    navbar();
+    var curUser = $cookies.getObject("user").full.split(",");
+    var curUserName = $cookies.getObject("user").userName.split(",");
+    $scope.user = curUser.pop();
+    $http.get("http://localhost:3005/getUser?name=" + curUserName)
+      .then((resp) => {
+        $scope.user = resp.data[0];
+        if (resp.data[0].profilePic === "") {
+          $scope.user.profilePic = "https://image.freepik.com/free-icon/user-male-silhouette_318-55563.jpg";
+        }
+      });
+
+    function getCurFriends() {
       $http.get("http://localhost:3005/getUser?name=" + curUserName)
-         .then((resp) => {
-            $scope.user = resp.data[0];
-            if (resp.data[0].profilePic === "") {
-               $scope.user.profilePic = "https://image.freepik.com/free-icon/user-male-silhouette_318-55563.jpg";
+        .then((resp) => {
+          $scope.friends = resp.data[0].friends;
+          if (resp.data[0].profilePic === "") {
+            $scope.userImage = "https://image.freepik.com/free-icon/user-male-silhouette_318-55563.jpg";
+          } else {
+            $scope.userImage = resp.data[0].profilePic;
+            $scope.foundPhoto = true;
+          }
+        });
+    }
+
+    var bigObj = {};
+    $http.get("http://localhost:3005/allUsers").then(function (resp) {
+      var lunchBoxUsers = [];
+      resp.data.forEach(function (ele) {
+        bigObj[ele.fullName] = ele.username;
+        lunchBoxUsers.push(ele.fullName);
+      });
+      $("#friendSearch").autocomplete({
+        source: lunchBoxUsers
+      });
+    }); // for the autocomplete feature
+
+    $scope.addFriend = function () {
+      $http.get(baseUrl + "getUser?name=" + bigObj[$scope.friendSearch])
+        .then((resp) => {
+          $http.put(baseUrl + "addFriend", {
+            user: $cookies.getObject("user").userName,
+            friend: {
+              fullName: resp.data[0].fullName,
+              username: resp.data[0].username,
+              email: resp.data[0].email
             }
-         });
+          }).then(function (resp) {
+            getCurFriends();
+            $scope.friendSearch = "";
+          });
+        });
+    };
+    getCurFriends();
+    console.log("poop")
 
-      function getCurFriends() {
-         $http.get("http://localhost:3005/getUser?name=" + curUserName)
-            .then((resp) => {
-               $scope.friends = resp.data[0].friends;
-               if (resp.data[0].profilePic === "") {
-                  $scope.userImage = "https://image.freepik.com/free-icon/user-male-silhouette_318-55563.jpg";
-               } else {
-                  $scope.userImage = resp.data[0].profilePic;
-                  $scope.foundPhoto = true;
-               }
-            });
-      }
+    $scope.getImage = function () {
+      var githubApi = "https://api.github.com/users/";
+      $http.get(githubApi + $scope.githubUsername).then(function (result) {
+        var data = result.data;
+        if (result.data !== undefined) {
+          $scope.userImage = data.avatar_url;
+          $scope.foundPhoto = true;
+        }
+        $http.put("http://localhost:3005/photo", {
+          userName: curUserName[0],
+          picture: data.avatar_url
+        }).then((resp) => console.log(resp));
+      });
+    };
 
-      var bigObj = {};
-      $http.get("http://localhost:3005/allUsers").then(function (resp) {
-         var lunchBoxUsers = [];
-         resp.data.forEach(function (ele) {
-            bigObj[ele.fullName] = ele.username;
-            lunchBoxUsers.push(ele.fullName);
-         });
-         $("#friendSearch").autocomplete({
-            source: lunchBoxUsers
-         });
-      }); // for the autocomplete feature
-
-      $scope.addFriend = function () {
-         $http.get(baseUrl + "getUser?name=" + bigObj[$scope.friendSearch])
-            .then((resp) => {
-               $http.put(baseUrl + "addFriend", {
-                  user: $cookies.getObject("user").userName,
-                  friend: {
-                     fullName: resp.data[0].fullName,
-                     username: resp.data[0].username,
-                     email: resp.data[0].email
-                  }
-               }).then(function (resp) {
-                  getCurFriends();
-                  $scope.friendSearch = "";
-               });
-            });
-      };
-      getCurFriends();
-
-
-      $scope.getImage = function () {
-         var githubApi = "https://api.github.com/users/";
-         $http.get(githubApi + $scope.githubUsername).then(function (result) {
-            var data = result.data;
-            if (result.data !== undefined) {
-               $scope.userImage = data.avatar_url;
-               $scope.foundPhoto = true;
+    function getFoodFaves() {
+      $http.get("http://localhost:3005/getUser?name=" + curUserName)
+        .then((response) => {
+          $scope.favorites = response.data[0].favorites;
+        });
+    }
+    bigObj = {};
+    $http.get("http://localhost:3005/allUsers").then(function (resp) {
+      var lunchBoxUsers = [];
+      resp.data.forEach(function (ele) {
+        bigObj[ele.fullName] = ele.username;
+        lunchBoxUsers.push(ele.fullName);
+      });
+      $("#friendSearch").autocomplete({
+        source: lunchBoxUsers
+      });
+    }); // for the autocomplete feature
+    $scope.addFriend = function () {
+      $http.get("http://localhost:3005/getUser?name=" + bigObj[$scope.friendSearch])
+        .then((resp) => {
+          $http.put("http://localhost:3005/addFriend", {
+            user: $cookies.getObject("user").userName,
+            friend: {
+              fullName: resp.data[0].fullName,
+              username: resp.data[0].username,
+              email: resp.data[0].email
             }
-            $http.put("http://localhost:3005/photo", {
-               userName: curUserName[0],
-               picture: data.avatar_url
-            }).then((resp) => console.log(resp));
-         });
-      };
+          }).then(function (resp) {
+            getCurFriends();
+            $scope.friendSearch = "";
+          });
+        });
+    };
 
-      function getFoodFaves() {
-         $http.get("http://localhost:3005/getUser?name=" + curUserName)
-            .then((response) => {
-               $scope.favorites = response.data[0].favorites;
-            });
-      }
-      bigObj = {};
-      $http.get("http://localhost:3005/allUsers").then(function (resp) {
-         var lunchBoxUsers = [];
-         resp.data.forEach(function (ele) {
-            bigObj[ele.fullName] = ele.username;
-            lunchBoxUsers.push(ele.fullName);
-         });
-         $("#friendSearch").autocomplete({
-            source: lunchBoxUsers
-         });
-      }); // for the autocomplete feature
-      $scope.addFriend = function () {
-         $http.get("http://localhost:3005/getUser?name=" + bigObj[$scope.friendSearch])
-            .then((resp) => {
-               $http.put("http://localhost:3005/addFriend", {
-                  user: $cookies.getObject("user").userName,
-                  friend: {
-                     fullName: resp.data[0].fullName,
-                     username: resp.data[0].username,
-                     email: resp.data[0].email
-                  }
-               }).then(function (resp) {
-                  getCurFriends();
-                  $scope.friendSearch = "";
-               });
-            });
-      };
+    $scope.addFavorite = function () {
+      $http.put("http://localhost:3005/addFavorite", {
+        name: $cookies.getObject("user").userName,
+        place: {
+          name: $scope.foodfav
+        }
+      }).then(function (resp) {
+        getFoodFaves();
+        $scope.foodfav = "";
+      });
+    };
 
-      $scope.addFavorite = function () {
-         $http.put("http://localhost:3005/addFavorite", {
-            name: $cookies.getObject("user").userName,
-            place: {
-               name: $scope.foodfav
-            }
-         }).then(function (resp) {
-            getFoodFaves();
-            $scope.foodfav = "";
-         });
-      };
-
-      getFoodFaves();
-      getCurFriends();
-   });
+    getFoodFaves();
+    getCurFriends();
+  });
